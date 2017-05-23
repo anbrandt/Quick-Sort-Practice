806dd2fbcb40800094857437d6b897cd578e79b5
anbrandt <andrzej.brandt@gmail.com>
2017-05-23 19:45:03 +0200

    another commit

--- src/com/andrzej/Main.java
+++ src/com/andrzej/Main.java
@@ -5,16 +5,23 @@ public class Main {
     public static void main(String[] args) {
 
 		int[] table1 = new int[] {1,44,32,21,6,8,12,33,44};
-//		System.out.println(table1[3]);
+		System.out.println("UNSORTED");
+		printTableElements(table1);
+
+		System.out.println("----------------");
 
 		QuickSort qsort2 = new QuickSort();
+		System.out.println("SORTED");
 		qsort2.sortTheTableFinalMethod(table1);
-		for (int i = 0; i < table1.length ; i++) {
-			System.out.println(table1[i]);
-		}
+		printTableElements(table1);
 
 	}
 
+	public static void printTableElements (int[] table) {
+		for (int i = 0; i < table.length; i++) {
+			System.out.println(table[i]);
+		}
+	}
 
 }
 //wybieramy jeden element z tablicy dowolny
